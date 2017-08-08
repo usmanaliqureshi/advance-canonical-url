c10c10f0314cba98d2d78fc1dc2d3062f39a4465
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-08 15:33:09 +0500

    Add: Basic plugin information

---
+++ advance-canonical-url.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: Advance Canonical URL
+ * Description: A WordPress plugin to avoid duplicate content throughout the website with advance settings.
+ * Plugin URI: https://github.com/usmanaliqureshi/advance-canonical-url
+ * Author: Usman Ali Qureshi
+ * Author URI: https://www.usmanaliqureshi.com
+ * Contributors: usmanaliqureshi
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: acu
+ * Domain Path: /languages/
+ */
+
