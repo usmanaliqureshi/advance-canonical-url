dd037be2ba03e5f943f73ae52519fc2c12380754
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-08 17:02:37 +0500

    Add: Base functionality

--- advance-canonical-url.php
+++ advance-canonical-url.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Plugin Name: Advance Canonical URL
  * Description: A WordPress plugin to avoid duplicate content throughout the website with advance settings.
@@ -12,3 +13,29 @@
  * Domain Path: /languages/
  */
 
+/**
+ * Intruders aren't allowed.
+ */
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
+}
+
+if ( ! class_exists( 'advance_canonical_url' ) ) {
+	class advance_canonical_url {
+		public function __construct() {
+			$this->acu_init();
+		}
+
+		public function acu_init() {
+			add_action( 'wp_head', array( $this, 'acu_render_canonical_url' ) );
+		}
+
+		public function acu_render_canonical_url() {
+			echo '<!-- Advance Canonical URL -->
+			<link rel="canonical" content="' . get_bloginfo( 'url' ) . '' . $_SERVER['REQUEST_URI'] . '">
+			<!-- Advance Canonical URL -->';
+		}
+	}
+}
+
+$ACU = new advance_canonical_url();
