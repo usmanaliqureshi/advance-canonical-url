5e6d0b0f2e45b9ce32e9480985758ee7e7150338
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-09 11:44:28 +0500

    Move: advance_canonical_url file to includes

---
+++ functions.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Plugin Name: Advance Canonical URL
+ * Description: A WordPress plugin to avoid duplicate content throughout the website with advance settings.
+ * Plugin URI: https://github.com/usmanaliqureshi/advance-canonical-url
+ * Author: Usman Ali Qureshi
+ * Author URI: https://www.usmanaliqureshi.com
+ * Contributors: usmanaliqureshi
+ * Version: 1.0.0
+ * License: GPL2
+ * Text Domain: acu
+ * Domain Path: /languages/
+ */
+
+require_once('includes/canonical-meta-box.php');
+
+require_once('includes/advance-canonical-url.php');
--- includes/advance-canonical-url.php
+++ includes/advance-canonical-url.php
@@ -1,18 +1,4 @@
 <?php
-
-/**
- * Plugin Name: Advance Canonical URL
- * Description: A WordPress plugin to avoid duplicate content throughout the website with advance settings.
- * Plugin URI: https://github.com/usmanaliqureshi/advance-canonical-url
- * Author: Usman Ali Qureshi
- * Author URI: https://www.usmanaliqureshi.com
- * Contributors: usmanaliqureshi
- * Version: 1.0.0
- * License: GPL2
- * Text Domain: acu
- * Domain Path: /languages/
- */
-
 /**
  * Intruders aren't allowed.
  */
@@ -173,6 +159,7 @@ if ( ! class_exists( 'advance_canonical_url' ) ) {
 
 		/**
 		 * Sanitization & Validation of the option
+		 *
 		 * @param $acu_input
 		 */
 		public function acu_sanitize_and_validate( $acu_input ) {
