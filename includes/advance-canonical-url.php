dd33de615a4257516f63b0f1391a4efa44a69c05
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2017-08-09 20:04:40 +0500

    Remove: Unnecessary WordPress action

--- includes/advance-canonical-url.php
+++ includes/advance-canonical-url.php
@@ -30,7 +30,6 @@ if (!class_exists('advance_canonical_url')) {
             if (is_admin()) {
                 add_action('admin_menu', array($this, 'acu_plugin_page'));
                 add_action('admin_init', array($this, 'acu_settings_page_init'));
-                add_action('update_option_acu_options', array($this, 'acu_update_options'), 10, 2);
             }
 
             remove_action('wp_head', 'rel_canonical');
