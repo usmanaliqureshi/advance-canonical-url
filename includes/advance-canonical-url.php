22a7784481eabb98a1e396bcb98eab926eddb1f3
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2023-03-05 17:31:40 +0500

    Code formatted

--- includes/advance-canonical-url.php
+++ includes/advance-canonical-url.php
@@ -283,7 +283,7 @@ if ( ! class_exists( 'advance_canonical_url' ) ) {
                     break;
 
                 case (is_page()):
-                    $this->acu_render_canonical_url($basic, $advance, $value);
+	                $this->acu_render_canonical_url( $basic, $advance, $value );
                     break;
 
                 default:
