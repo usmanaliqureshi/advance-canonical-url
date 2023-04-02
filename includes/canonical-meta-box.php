02a2f31dc2341d61623c58da246ba2ca2b712bd2
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2023-04-02 13:39:29 +0500

    Code formatted

--- includes/canonical-meta-box.php
+++ includes/canonical-meta-box.php
@@ -154,7 +154,7 @@ if ( ! class_exists( 'advance_canonical_meta_box' ) ) {
             /**
              * Use get_post_meta to retrieve an existing value from the database.
              */
-            $value = get_post_meta($post->ID, '_acu_can_url_value', true);
+	        $value = get_post_meta( $post->ID, '_acu_can_url_value', true );
 
             $default_can_url = get_permalink();
 
