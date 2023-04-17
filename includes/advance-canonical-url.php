b680bc1d089e012d6e2954dec2361dac321d2567
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2023-04-17 21:51:12 +0500

    Code formatted

--- includes/advance-canonical-url.php
+++ includes/advance-canonical-url.php
@@ -20,8 +20,7 @@ if ( ! class_exists( 'advance_canonical_url' ) ) {
         /**
          * Initialization
          */
-        public function acu_init()
-        {
+        public function acu_init() {
 
 	        register_activation_hook( __FILE__, array( $this, 'acu_activation' ) );
 	        register_deactivation_hook( __FILE__, array( $this, 'acu_deactivation' ) );
