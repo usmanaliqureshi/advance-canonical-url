ade20a025c346a457bc6a5ead1b8e766008d062d
Usman Ali Qureshi <usman@usmanaliqureshi.com>
2023-06-07 23:39:30 +0500

    Code Formatted

--- includes/advance-canonical-url.php
+++ includes/advance-canonical-url.php
@@ -259,25 +259,25 @@ if ( ! class_exists( 'advance_canonical_url' ) ) {
 	        $advance .= '<!-- Advance Canonical URL -->';
 
 	        switch ( true ) {
-	            case ( is_front_page() ):
-                    echo $basic;
-                    break;
+		        case ( is_front_page() ):
+			        echo $basic;
+			        break;
 
 		        case ( is_home() ):
 			        echo $basic;
 			        break;
 
 		        case ( is_single() ):
-	                $this->acu_render_canonical_url( $basic, $advance, $value );
-                    break;
+			        $this->acu_render_canonical_url( $basic, $advance, $value );
+			        break;
 
 		        case ( is_page() ):
-	                $this->acu_render_canonical_url( $basic, $advance, $value );
-                    break;
+			        $this->acu_render_canonical_url( $basic, $advance, $value );
+			        break;
 
-                default:
-	                $this->acu_render_canonical_url( $basic, $advance, $value );
-            }
+		        default:
+			        $this->acu_render_canonical_url( $basic, $advance, $value );
+	        }
         }
 
         /**
